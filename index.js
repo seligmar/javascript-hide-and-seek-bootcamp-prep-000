342f55a9c1c5018df5999f8f2db0169ee7ec5f9b
Mary Selig <seligmar@gmail.com>
2019-01-31 17:25:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+// Define a function getFirstSelector(selector), which accepts a selector and returns the first element that matches.
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector); 
+}
+
+function nestedTarget() {
+  return document.getElementById("nested").querySelector('.target'); 
+}
+
+
+function deepestChild() {
+  var node = document.getElementById('grand-node').querySelectorAll('div');
+  for (let i = 0; i < node.length; i++) {
+      var fizz = node[i]  
+  } 
+  return fizz; 
+}
+
+function increaseRankBy(n) {
+  var increasedRank = document.getElementById('app').querySelectorAll(".ranked-list");  
+  for (let i = 0; i < increasedRank.length; i++) {
+    increasedRank[i].innerHTML = parseInt(increasedRank[i] + n);   
+  }
+  return increasedRank; 
+}
